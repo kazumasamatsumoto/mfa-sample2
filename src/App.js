532c89f604bc0a5f1e66baf5c8262d52b0ca79a0
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-02-15 17:03:59 +0900

    onAuthStateChangedを追加

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import { logOut } from "./firebase/signOut.js";
 import { signInMethod } from "./firebase/signIn.js";
 import { twoFactorMethod } from "./firebase/twoFactor.js";
 import { verifyAction } from "./firebase/verifyAction.js";
-import { loginForm } from "./firebase/login.js"
-import { loginVerifyAction } from "./firebase/loginVerifyAction.js"
+import { loginForm } from "./firebase/login.js";
+import { loginVerifyAction } from "./firebase/loginVerifyAction.js";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebase.js";
+
+onAuthStateChanged(auth, (user) => {
+  console.debug("onAuthStateChanged");
+  if (user) {
+    console.debug("signIn");
+  } else {
+    console.debug("signOut");
+  }
+});
 
 function App() {
   const [mobile, setMobile] = useState("");
@@ -32,7 +43,9 @@ function App() {
         required
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={() => signInMethod(email, password)}>アカウント作成</button>
+      <button onClick={() => signInMethod(email, password)}>
+        アカウント作成
+      </button>
       <h4>アカウント２段階認証Form</h4>
       <h4>電話番号を入力すると認証コードが届きます。0を抜いてください</h4>
       <div id="sign-in-button"></div>
@@ -69,7 +82,9 @@ function App() {
         required
         onChange={(e) => setLoginPassword(e.target.value)}
       />
-      <button onClick={() => loginForm(loginEmail, loginPassword)}>ログイン</button>
+      <button onClick={() => loginForm(loginEmail, loginPassword)}>
+        ログイン
+      </button>
       <h4>ログイン認証コード入力</h4>
       <div id="sign-in-button"></div>
       <input
@@ -79,7 +94,9 @@ function App() {
         required
         onChange={(e) => setLoginOpt(e.target.value)}
       />
-      <button onClick={() => loginVerifyAction(loginOpt)}>認証コード入力</button>
+      <button onClick={() => loginVerifyAction(loginOpt)}>
+        認証コード入力
+      </button>
     </div>
   );
 }
